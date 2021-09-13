159bac8a481c7213d594a2a2d8aceb649d4e6f6e
renkeles <84261162+renkeles@users.noreply.github.com>
2021-09-13 14:01:00 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,54 +2,63 @@
 #include <string>
 
 template <class T>
-class Pair1{
+class Pair1 {
 private:
     T p_first;
     T p_second;
 public:
-    Pair1(T first, T second) : p_first(first), p_second(second){
+    Pair1(T first, T second) : p_first(first), p_second(second) {
 
     }
-    T first(){
+    T first() {
         return p_first;
     }
-    T second(){
+    T second() {
         return p_second;
     }
 };
-void task1(){
-    Pair1<int> p1(6,9);
+void task1() {
+    Pair1<int> p1(6, 9);
     std::cout << "Pair: " << p1.first() << " " << p1.second() << std::endl;
 
-    Pair1<double> p2(3.4,7.8);
+    Pair1<double> p2(3.4, 7.8);
     std::cout << "Pair: " << p2.first() << " " << p2.second() << std::endl;
 }
 
-template <class T, class M>
-class Pair{
+template <class T1, class T2>
+class Pair {
 private:
-    T p_first;
-    M p_second;
+    T1 p_first;
+    T2 p_second;
 public:
-    Pair(T first, M second) : p_first(first), p_second(second){
+    Pair(T1 first, T2 second) : p_first(first), p_second(second) {
 
     }
-    T first(){
+    T1 first() {
         return p_first;
     }
-    M second(){
+    T2 second() {
         return p_second;
     }
 
 };
-void task2(){
-    Pair<int, double> p1(6,7.8);
+void task2() {
+    Pair<int, double> p1(6, 7.8);
     std::cout << "Pair: " << p1.first() << " " << p1.second() << std::endl;
 
     Pair<double, int> p2(3.4, 5);
     std::cout << "Pair: " << p2.first() << " " << p2.second() << std::endl;
 }
 
+template<class T>
+class StringValuePair : public Pair<std::string, T> {
+public:
+    StringValuePair(const std::string &str, const T &value) : Pair<std::string, T>(str, value)  {}
+};
+void task3() {
+    StringValuePair<int> svp("Amazing", 7);
+    std::cout << "Pair: " << svp.first() << ' ' << svp.second() << '\n';
+}
 
 
 
@@ -57,6 +66,7 @@ int main() {
 
     //task1();
     //task2();
+    task3();
 
 
     return 0;
